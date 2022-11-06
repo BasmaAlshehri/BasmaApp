6dfd6f0eededc56beb25e212d1951386bce964bf
BasmaAlshehri <116463274+BasmaAlshehri@users.noreply.github.com>
2022-11-06 23:36:50 +0300

    Add files via upload
    The Code file

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/',(req, res) => {
+    res.send('hello world')
+})
+app.get('/callback' , (req, res) => {
+    res.send('it should have worked!')
+})
+
+app.listen(3000, () => {
+
+    console.log('we are running on port 3000')
+})
